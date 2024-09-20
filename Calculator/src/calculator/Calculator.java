8c7e645ccd64b5d5b0227d6286696edb5ce19878
eloise muller <elois@10.3.61.253>
2024-09-20 18:43:47 +0200

    Création d'un nouveau projet versionné pour TP0 calculator

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -1,8 +1,13 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * eloise muller 
+ * Calculette TP0
+TDC
+Septembre 2024
+
  */
 package calculator;
+
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +19,32 @@ public class Calculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        System.out.println("Please enter the operator:");
+        System.out.println("1) add");
+        System.out.println("2) substract");
+        System.out.println("3) multiply");
+        System.out.println("4) divide");
+        System.out.println("5) modulo");
+        
+  int operateur;
+  int operande1;
+  int operande2;
+  int result;
+Scanner sc = new Scanner(System.in);
+operateur =sc.nextInt(); // On demande a sc de donner le prochain entier
+
+          System.out.println("saisisez une valeur");
+        operande1 =sc.nextInt();
+        System.out.println("saisisez une valeur");
+        operande2 =sc.nextInt();
+        if (operateur == 1);
+        result = operande1 + operande2;
+        System.out.println("le resultat est :");
+        
+       
+        
+        
     }
     
 }
