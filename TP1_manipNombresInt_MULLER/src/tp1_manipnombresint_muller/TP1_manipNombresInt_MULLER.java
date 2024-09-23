3d70502ab4ee44174429ac7386118186881d944e
eloise muller <elois@10.3.61.253>
2024-09-23 12:13:57 +0200

    Création d'un nouveau projet versionné pour TP1 TP1 PARTIE 1

--- TP1_manipNombresInt_MULLER/src/tp1_manipnombresint_muller/TP1_manipNombresInt_MULLER.java
+++ TP1_manipNombresInt_MULLER/src/tp1_manipnombresint_muller/TP1_manipNombresInt_MULLER.java
@@ -1,8 +1,12 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * elooise muller 
+tdb 
+septembre 2024
+TP1 part 1
  */
 package tp1_manipnombresint_muller;
+
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +18,22 @@ public class TP1_manipNombresInt_MULLER {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+Scanner sc = new Scanner(System.in);
+System.out.println("Entrer deux entiers :");
+        int nombre1= sc.nextInt();
+        int nombre2= sc.nextInt();
+// on initialise les variables ( Q2)
+int resultatsomme = nombre1 + nombre2 ;
+int resultatdiff = nombre1 - nombre2;
+int resultatproduit = nombre1 * nombre2;
+int resultatdiv = nombre1 / nombre2;
+int resultatmodulo = nombre1 % nombre2;
+//affichage des résultats 
+System.out.println("Le resultat de la somme des deux entiers est:" + resultatsomme );
+System.out.println("Le resultat de la difference des deux entiers est:" + resultatdiff );       
+System.out.println("Le resultat du produit des deux entiers est:" + resultatproduit );
+System.out.println("Le resultat du quotient des deux entiers est:" + resultatdiv );
+System.out.println("Le resultat du reste de la division euclidienne des deux entiers est:" + resultatmodulo );
     }
     
 }
