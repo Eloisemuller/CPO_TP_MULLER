154e87852605aec189e4e0a21edf18a7bb9f6666
eloise muller <elois@10.3.61.253>
2024-09-26 23:14:38 +0200

    Création d'un nouveau projet versionné pour TP0

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -1,7 +1,7 @@
 /*
  * eloise muller 
  * Calculette TP0
-TDC
+TDb
 Septembre 2024
 
  */
@@ -19,6 +19,7 @@ public class Calculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Please enter the operator:");
         System.out.println("1) add");
@@ -27,23 +28,80 @@ public class Calculator {
         System.out.println("4) divide");
         System.out.println("5) modulo");
         
-  int operateur;
-  int operande1;
-  int operande2;
-  int result;
-Scanner sc = new Scanner(System.in);
-operateur =sc.nextInt(); // On demande a sc de donner le prochain entier
+ // int operateur;
+  //int operande1;
+//  int operande2;
+//  int result;
+int operator = scanner.nextInt(); // On demande a sc de donner le prochain entier
+if (operator < 1 || operator > 5) {
+            System.out.println("Error: Invalid operator selected. Please choose a number between 1 and 5.");
+        } else {
+            // Demander à l'utilisateur une première valeur (operande1)
+            System.out.println("Please enter the first operand:");
+            double operande1 = scanner.nextDouble();
 
-          System.out.println("saisisez une valeur");
-        operande1 =sc.nextInt();
-        System.out.println("saisisez une valeur");
-        operande2 =sc.nextInt();
-        if (operateur == 1);
-        result = operande1 + operande2;
-        System.out.println("le resultat est :");
-        
-       
-        
+            // Demander à l'utilisateur une deuxième valeur (operande2)
+            System.out.println("Please enter the second operand:");
+            double operande2 = scanner.nextDouble();
+
+            // Variable pour stocker le résultat
+            double result = 0;
+
+            // Calcul du résultat en fonction de l'opérateur
+            switch (operator) {
+                case 1: // Addition
+                    result = operande1 + operande2;
+                    System.out.println("The result of addition is: " + result);
+                    break;
+                case 2: // Soustraction
+                    result = operande1 - operande2;
+                    System.out.println("The result of subtraction is: " + result);
+                    break;
+                case 3: // Multiplication
+                    result = operande1 * operande2;
+                    System.out.println("The result of multiplication is: " + result);
+                    break;
+                case 4: // Division
+                    if (operande2 != 0) {
+                        result = operande1 / operande2;
+                        System.out.println("The result of division is: " + result);
+                    } else {
+                        System.out.println("Error: Division by zero is not allowed.");
+                    }
+                    break;
+                case 5: // Modulo
+                    if (operande2 != 0) {
+                        result = operande1 % operande2;
+                        System.out.println("The result of modulo is: " + result);
+                    } else {
+                        System.out.println("Error: Modulo by zero is not allowed.");
+                    }
+                    break;
+                default:
+                    System.out.println("Error: Invalid operator selected.");
+                    break;
+            }
+        }
+
+        // Fermeture du scanner
+  
+
+    //    System.out.println("saisisez une valeur");
+    //    operande1 =sc.nextInt();
+      //  System.out.println("saisisez une valeur");
+        //operande2 =sc.nextInt();
+//        if (operateur < 1 || operateur > 5) {
+  //          System.out.println("erreur.");
+    //        System.exit(0);
+      //  }
+        //System.out.println("s'il vous plait entrez le premier numero:");
+ //       int operande1 = scanner.nextInt();
+
+//        System.out.println("s'il vous plait entrez le second numero:");
+  //      int operande2 = scanner.nextInt();
+//
+  //     
+         
         
     }
     
