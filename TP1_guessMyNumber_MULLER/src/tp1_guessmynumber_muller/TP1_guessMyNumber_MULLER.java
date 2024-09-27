a809fe46cca647c26d9277d67eb89458a8f8ad3a
eloise muller <elois@10.3.61.253>
2024-09-27 11:12:30 +0200

    Création d'un nouveau projet versionné pour TP1 TP1 partie Guess my number

--- TP1_guessMyNumber_MULLER/src/tp1_guessmynumber_muller/TP1_guessMyNumber_MULLER.java
+++ TP1_guessMyNumber_MULLER/src/tp1_guessmynumber_muller/TP1_guessMyNumber_MULLER.java
@@ -4,6 +4,9 @@
  */
 package tp1_guessmynumber_muller;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author elois
@@ -14,7 +17,38 @@ public class TP1_guessMyNumber_MULLER {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         Random generateurAleat = new Random();
+        
+        // Affichage de 5 nombres aléatoires entre 0 et 100
+        System.out.println("Voici 5 nombres aléatoires entre 0 et 100 :");
+        for (int i = 0; i < 5; i++) {
+            int n = generateurAleat.nextInt(100); // Génère un nombre aléatoire entre 0 et 99
+            System.out.println(n);
+        }
+ // Q2 Génération d'un nombre aléatoire entre 0 et 100
+        int nombreAleatoire = generateurAleat.nextInt(100);
+        
+        // Création d'un objet Scanner pour lire les entrées utilisateur
+        Scanner scanner = new Scanner(System.in);
+        
+        // Variable pour stocker la saisie de l'utilisateur
+        int saisieUtilisateur = 1;
+        
+        // Boucle tant que l'utilisateur n'a pas trouvé le bon nombre
+        while (saisieUtilisateur != nombreAleatoire) {
+            // Demande à l'utilisateur de saisir un nombre entre 0 et 100
+            System.out.print("Saisissez un nombre entre 0 et 100 : ");
+            saisieUtilisateur = scanner.nextInt();
+            
+            // Comparaison de la saisie avec le nombre aléatoire
+            if (saisieUtilisateur < nombreAleatoire) {
+                System.out.println("Trop petit !");
+            } else if (saisieUtilisateur > nombreAleatoire) {
+                System.out.println("Trop grand !");
+            } else {
+                System.out.println("Gagné !");
+            }
+        }
     }
     
 }
